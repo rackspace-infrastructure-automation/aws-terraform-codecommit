d8c6c77e9367bf5371cd02240615cd51a8bdfb04
Giovanni Del Valle <giovanni.delvalle@rackspace.com>
2020-02-26 21:14:16 -0600

    style guide updates

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ module "codecommit_repo" {
  * Full working references are available at [examples](examples)
  */
 
- terraform {
+terraform {
   required_version = ">= 0.12"
 
-    required_providers {
+  required_providers {
     aws = ">= 2.7.0"
   }
 }
@@ -30,58 +30,67 @@ resource "aws_codecommit_repository" "repo" {
 }
 
 resource "aws_codecommit_trigger" "trigger_1" {
-  count           = var.enable_trigger_1 ? 1 : 0
-  depends_on      = [aws_codecommit_repository.repo]
+  count = var.enable_trigger_1 ? 1 : 0
+
   repository_name = var.repository_name
 
   trigger {
-    name            = var.trigger_1_name
-    events          = var.trigger_1_events
-    destination_arn = var.trigger_1_destination_arn
-    custom_data     = var.trigger_1_custom_data
     branches        = var.trigger_1_branches
+    custom_data     = var.trigger_1_custom_data
+    destination_arn = var.trigger_1_destination_arn
+    events          = var.trigger_1_events
+    name            = var.trigger_1_name
   }
+
+  depends_on = [aws_codecommit_repository.repo]
 }
 
+
 resource "aws_codecommit_trigger" "trigger_2" {
-  count           = var.enable_trigger_2 ? 1 : 0
-  depends_on      = [aws_codecommit_repository.repo]
+  count = var.enable_trigger_2 ? 1 : 0
+
   repository_name = var.repository_name
 
   trigger {
-    name            = var.trigger_2_name
-    events          = var.trigger_2_events
-    destination_arn = var.trigger_2_destination_arn
-    custom_data     = var.trigger_2_custom_data
     branches        = var.trigger_2_branches
+    custom_data     = var.trigger_2_custom_data
+    destination_arn = var.trigger_2_destination_arn
+    events          = var.trigger_2_events
+    name            = var.trigger_2_name
   }
+
+  depends_on = [aws_codecommit_repository.repo]
 }
 
 resource "aws_codecommit_trigger" "trigger_3" {
-  count           = var.enable_trigger_3 ? 1 : 0
-  depends_on      = [aws_codecommit_repository.repo]
+  count = var.enable_trigger_3 ? 1 : 0
+
   repository_name = var.repository_name
 
   trigger {
-    name            = var.trigger_3_name
-    events          = var.trigger_3_events
-    destination_arn = var.trigger_3_destination_arn
-    custom_data     = var.trigger_3_custom_data
     branches        = var.trigger_3_branches
+    custom_data     = var.trigger_3_custom_data
+    destination_arn = var.trigger_3_destination_arn
+    events          = var.trigger_3_events
+    name            = var.trigger_3_name
   }
+
+  depends_on = [aws_codecommit_repository.repo]
 }
 
 resource "aws_codecommit_trigger" "trigger_4" {
-  count           = var.enable_trigger_4 ? 1 : 0
-  depends_on      = [aws_codecommit_repository.repo]
+  count = var.enable_trigger_4 ? 1 : 0
+
   repository_name = var.repository_name
 
   trigger {
-    name            = var.trigger_4_name
-    events          = var.trigger_4_events
-    destination_arn = var.trigger_4_destination_arn
-    custom_data     = var.trigger_4_custom_data
     branches        = var.trigger_4_branches
+    custom_data     = var.trigger_4_custom_data
+    destination_arn = var.trigger_4_destination_arn
+    events          = var.trigger_4_events
+    name            = var.trigger_4_name
   }
+
+  depends_on = [aws_codecommit_repository.repo]
 }
 
